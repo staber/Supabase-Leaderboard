d24d725ab11b1f63ad5d50767e6c668f2935d031
Adam Staber <staber@gmail.com>
2024-06-05 08:51:56 -0400

    https

--- src/pages/_2024.js
+++ src/pages/_2024.js
@@ -2,7 +2,7 @@ const _2024 = () => {
     return (
         <div class="vertical-center">
             <button>Continue...
-                <a href="http://lcfh2011.streamlit.app" target="_blank"></a>
+                <a href="https://lcfh2011.streamlit.app" target="_blank"></a>
             </button>
         </div>
     )
